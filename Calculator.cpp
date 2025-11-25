1bfc51b3723c641142de92c4e4dade72211778ff
Ameen ul Husnaat <ameenulhusnaat82119@gmail.com>
2025-11-25 19:56:49 +0500

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int a, b, add, sub, multip, div;
+	cout<<"Enter the value of a:";
+	cin>>a;
+	cout<<"Enter the value of b:";
+	cin>>b;
+	add = a + b;
+	cout<<"Addition of two numbers is  "<<add<<endl;
+	sub = a - b;
+	cout<<"Subtraction of two numbers is  "<<sub<<endl;
+	multip = a * b;
+	cout<<"Multiplication of two numbers is  "<<multip<<endl;
+	div = a / b;
+	cout<<"Division of two numbers is  "<<div<<endl;
+	return 0;
+}
